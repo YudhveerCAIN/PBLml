4e904e13d2ff139984ead31acd2c69015fe02aba
yudhveerCAIN <yudhveerdeswal12@gmail.com>
2026-03-17 02:25:48 +0530

    added new api end points for dashborad

--- backend/main.py
+++ backend/main.py
@@ -179,3 +179,92 @@ def collect_data(data: SessionData):
             "status": "error",
             "message": str(e)
         }
+    
+
+#analytics endpoint
+    
+@app.get("/analytics/overview")
+def overview():
+
+    total_sessions = sessions_collection.count_documents({})
+    total_predictions = predictions_collection.count_documents({})
+    bots = predictions_collection.count_documents({"prediction": 1})
+    humans = predictions_collection.count_documents({"prediction": 0})
+
+    bot_rate = (bots / total_predictions * 100) if total_predictions else 0
+
+    return {
+        "total_sessions": total_sessions,
+        "total_predictions": total_predictions,
+        "bots": bots,
+        "humans": humans,
+        "bot_rate": bot_rate
+    }
+
+@app.get("/analytics/sites")
+def site_stats():
+
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$site_id",
+                "sessions": {"$sum": 1}
+            }
+        }
+    ]
+
+    sites = list(sessions_collection.aggregate(pipeline))
+
+    return sites
+
+@app.get("/analytics/live-sessions")
+def live_sessions():
+
+    active = []
+
+    for session_id, events in session_buffers.items():
+
+        active.append({
+            "session_id": session_id,
+            "event_count": len(events)
+        })
+
+    return {
+        "active_sessions": len(session_buffers),
+        "sessions": active
+    }
+
+@app.get("/analytics/recent-detections")
+def recent():
+
+    data = list(
+        predictions_collection
+        .find({})
+        .sort("updated_at",-1)
+        .limit(20)
+    )
+
+    for d in data:
+        d["_id"] = str(d["_id"])
+
+    return data
+
+@app.get("/analytics/bot-probabilities")
+def bot_probabilities():
+
+    pipeline = [
+        {
+            "$bucket": {
+                "groupBy": "$bot_probability",
+                "boundaries": [0,0.2,0.4,0.6,0.8,1],
+                "default": "other",
+                "output": {
+                    "count": {"$sum":1}
+                }
+            }
+        }
+    ]
+
+    result = list(predictions_collection.aggregate(pipeline))
+
+    return result
